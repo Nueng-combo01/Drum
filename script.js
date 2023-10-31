5bc2511718e2f5eef8e9abf19655ba7e76d0acc1
Bannason Thongaon <138765158+Nueng-combo01@users.noreply.github.com>
2023-10-31 21:05:27 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -11,6 +11,30 @@ $(document).ready(function() {
     });
 });
 
-document.querySelector("f").addEventListener("click", () => {
-    alert("Sound");
+  const sounds = {
+    s: new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'),
+    f: new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'),
+    b: new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'),
+    t: new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'),
+    h: new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'),
+    c: new Audio('https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'),
+  };
+
+  function playSound(key) {
+    if (sounds[key]) {
+      sounds[key].currentTime = 0;
+      sounds[key].play();
+    }
+  }
+
+  document.querySelectorAll('.drum button').forEach((button) => {
+    const key = button.getAttribute('data-key');
+    button.addEventListener('click', () => playSound(key));
+  });
+
+  document.addEventListener('keydown', (event) => {
+    const key = event.key.toLowerCase();
+    if (sounds[key]) {
+      playSound(key);
+    }
   });
